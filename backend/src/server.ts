031d92ebf5e490e76837d8d01af6c3fab76f9395
essadeq billouche <essadeq.billouche@gmail.com>
2025-12-09 17:10:42 +0100

    refactor:  ADSDT-21 clean up dotenv configuration and update server startup logic

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,17 @@
+import dotenv from "dotenv";
+dotenv.config(); // Load env FIRST
+
 import app from "./app.js";
+import { connectionDB } from "./config/db.js";
 
+const PORT = process.env.PORT || 3000;
 
+const startServer = async () => {
+  await connectionDB();
+  
+  app.listen(PORT, () => {
+    console.log(`<<< Server is running port : ${PORT} >>>`);
+  });
+};
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running on port ${process.env.PORT}`);
-});
+startServer();
