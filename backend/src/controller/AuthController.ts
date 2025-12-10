b01607715aee9a1c3b5afbab6169c123a8f00c01
essadeq billouche <essadeq.billouche@gmail.com>
2025-12-10 15:15:20 +0100

    feat: ADSDT-24 enhance register method to validate all required fields and improve error handling

--- backend/src/controller/AuthController.ts
+++ backend/src/controller/AuthController.ts
@@ -1,6 +1,7 @@
 import type { Request, Response, NextFunction } from "express";
 import type { registerDTO } from "../dto/auth/AuthDTO.js";
 import { AuthService } from "../service/AuthService.js";
+import { ErrorHandler } from "../helper/ErrorHandler.js";
 
 export class AuthController {
   
@@ -10,16 +11,14 @@ export class AuthController {
         try {
             const { name, email, password, role } = req.body;
 
-            if (!email || !password || !name) {
-                res.status(400).json({ message: "Missing required fields" });
-                return;
+            if (!email || !password || !name || !role) {
+                throw new ErrorHandler('missing information ', 400)
             }
 
             const user = await this.authService.register({ name, email, password, role } as registerDTO);
-
             res.status(201).json({ success: true, data: user });
         } catch (error) {
             next(error);
-        }
+        } 
     };
 }
