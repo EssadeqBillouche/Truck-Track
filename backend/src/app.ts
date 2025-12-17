13a9fb20404fa42e2eda749e0cea3471bbc9cf49
essadeq billouche <essadeq.billouche@gmail.com>
2025-12-17 13:23:38 +0100

    feat: add driver, truck, tire, and trailer API routes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,7 +3,12 @@ import cors from "cors";
 import morgan from "morgan";
 import authRoutes from "./routes/authRoutes.js";
 import adminRoutes from "./routes/adminRoutes.js";
+import truckRoutes from "./routes/truckRoutes.js";
 import { errorMiddleware } from "./middlewares/errorMiddleware.js";
+import tireRoutes from "./routes/tireRoutes.js";
+import trailerRoutes from "./routes/trailerRoutes.js";
+import driverRoutes from "./routes/driverRoutes.js";
+
 
 const app = express();
 
@@ -17,6 +22,10 @@ app.use(morgan("dev"));
 // routes
 app.use("/api-v1/auth", authRoutes);
 app.use("/api-v1/admin", adminRoutes);
+app.use("/api-v1/driver", driverRoutes);
+app.use("/api-v1/trucks", truckRoutes);
+app.use("/api-v1/tires", tireRoutes);
+app.use("/api-v1/trailers", trailerRoutes);
 
 // error middleware
 app.use(errorMiddleware);
